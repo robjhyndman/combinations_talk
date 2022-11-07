46ca0554c0234ccc4c6148c72f2986a7fccdf88d
Rob Hyndman <robjhyndman@gmail.com>
2022-11-07 18:29:08 +1100

    Working on crps plots

--- packages.R
+++ packages.R
@@ -1,1 +1,1 @@
-install.packages(c("transformr", "latex2exp", "magick","remotes","styler","binb","codetools","tidyverse","fpp3","distributional","gganimate"))
+install.packages(c("codetools","transformr", "latex2exp", "magick","remotes","styler","binb","codetools","tidyverse","fpp3","distributional","gganimate"))
