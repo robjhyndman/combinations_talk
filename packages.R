e19f307fc510ea8132e1c5daa173af11aa87ffbf
Rob Hyndman <robjhyndman@gmail.com>
2022-11-14 19:06:59 +1100

    Giving up on loop

--- packages.R
+++ packages.R
@@ -1,4 +1,3 @@
 install.packages(c("codetools","transformr", "latex2exp", "magick","remotes","styler","binb","codetools","tidyverse","fpp3","distributional","gganimate"))
 library(codetools)
 library(transformr)
-library(namedropR)
