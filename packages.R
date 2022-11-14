babf1169f12917bb3ec049a5d8c92e7311b6c3ff
Rob J Hyndman <robjhyndman@gmail.com>
2022-11-14 12:35:23 +1100

    Added covid section

--- packages.R
+++ packages.R
@@ -1,1 +1,4 @@
 install.packages(c("codetools","transformr", "latex2exp", "magick","remotes","styler","binb","codetools","tidyverse","fpp3","distributional","gganimate"))
+library(codetools)
+library(transformr)
+library(namedropR)
